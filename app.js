9d472bdd235069ba31630e2dde659248e1426850
LizzieGuajardo <32180546+LizzieGuajardo@users.noreply.github.com>
2019-04-08 18:03:36 -0500

    EX1

---
+++ app.js
@@ -0,0 +1,83 @@
+//Lizzie M. Guajardo Mozo
+//A00818258
+//EX 2
+
+const express = require('express')
+const path = require ('path')
+const met = require('./met.js')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+const publicDir = path.join(__dirname, 'public')
+app.use(express.static(publicDir))
+
+//http://localhost:3000
+app.get('/', function(req,res){
+	res.send('<h1>Examen Parcial 2 Desarrollo Web!<h1>')
+})
+
+//http://localhost:3000/students/A00818258
+app.get('/students/:id',function(req,res){
+	if(req.params.id == "A00818258"){
+		return res.send
+		({
+		  id: "A00818258",
+		  fullname: "Lizzie Marielle Guajardo Mozo",
+		  nickname: "Lizzie G",
+		  age: 21
+		})
+	}
+	else{
+		return res.send
+		({
+			error:"Matrícula no identificada. Intente con students/A00818258"
+		})
+	}	
+})
+
+//http://localhost:3000/met?search=sunflower
+app.get("/met", function(req,res){
+	//ESTA LINEA PERMITE QUE LLEGUEN REQUEST DE DONDE SEA, AUNQUE SEAN LOCALES
+	res.setHeader('Access-Control-Allow-Origin','*');
+	if(!req.query.search){
+		return res.send({
+			error: 'Tienes que dar un objeto o un autor'
+		})
+	}
+	else
+	{
+		met.searchObject(req.query.search, function(error, response){
+			if(error){
+				return res.send({
+					error: error
+				})
+			}
+			else
+			{
+				res.send({
+					objectName : response.objectName,
+				  	artist : response.artist,
+				  	title: response.title,
+				 	year: response.year,
+				  	technique: response.technique,
+				  	metUrl: response.metUrl
+				})
+			}	
+		})
+	}
+})
+
+//Por si se accesa a un pagina que no existe
+//http://localhost:3000/aboutasdfasdfasdfadf
+app.get('*',function(req,res){
+	res.send({
+		error: 'Esta ruta no existe'
+	})
+})
+
+//En el puerto 3000
+app.listen(port,function(){
+	console.log('up and running')
+})
---
+++ met.js
@@ -0,0 +1,87 @@
+//Lizzie M. Guajardo Mozo
+//A00818258
+//EX2
+
+const request = require ('request')
+
+//https://collectionapi.metmuseum.org/public/collection/v1/objects/1
+const displayPiece = function(objectID,searchTerm,callback){
+	const url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'+objectID
+	request({url:url, json:true}, function(error,response){
+		if(error){//no se puede acceder a la liga
+			callback('Service unavailabe', undefined)
+		}
+		//"message": "could not parse objectID"
+		else if (response.body.message == "could not parse objectID"){
+			callback('could not parse objectID', undefined)
+		}
+		else{
+			var constituents = "null"
+			var title = "null"
+			var year = "null"
+			var technique = "null"
+			var metUrl = "null"
+
+			if(response.body.constituents){ //Si no hay autor
+				constituents = response.body.constituents[0].name
+			}
+			if(response.body.title){ //Si no hay titulo
+				title = response.body.title
+			}
+			if(response.body.objectEndDate){ //Si no hay año
+				year = response.body.objectEndDate
+			}
+			if(response.body.medium){ //Si no hay tecnica
+				technique = response.body.mediums
+			}
+			if(response.body.objectURL){ //Si no hay url
+				metUrl = response.body.objectURL
+			}
+			
+			const info = 
+			{
+			  	objectName : searchTerm,
+			  	artist : constituents,			  	
+			  	title: title,
+			 	year: year,
+			  	technique: technique,
+			  	metUrl: metUrl
+			}
+			callback(undefined, info)
+		}
+	})
+}
+
+//https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers
+const searchObject = function(objectName,callback){ //
+	const url = 'https://collectionapi.metmuseum.org/public/collection/v1/search?q='+objectName
+	request({url:url, json:true}, function(error,response){
+		if(error){//no se puede acceder a la liga
+			callback('Service unavailabe', undefined) //
+		}
+		else if (response.body.total == 0 || !response.body.objectIDs){
+			callback('No se encontaron resultados', undefined)
+		}
+		else{
+			const data = response.body
+			const info = 
+			{
+				objectID : data.objectIDs[0]
+			}
+			displayPiece(info.objectID,objectName,function(error,response){
+				if(error)
+				{
+					callback(undefined, error)
+				}else
+				{
+					callback(undefined, response)
+				}
+			})
+		}
+	})
+}
+
+module.exports = {
+  searchObject: searchObject,
+  displayPiece: displayPiece
+}
